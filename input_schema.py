ec83c9db3e658e8b4bc5f03c5cf3c966f884fe8e
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-02-14 14:15:28 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -2,7 +2,10 @@ INPUT_SCHEMA = {
     "prompt": {
         'datatype': 'STRING',
         'required': True,
-        'shape': [1],
-        'example': ["What is Quantum Computing?"]
+        'shape': [-1],
+        'example': [
+                    "Write a story.",
+                    "What is Deep Learning?"
+                  ]
     }
 }
